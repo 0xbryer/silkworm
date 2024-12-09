5917a9cf8e5eeb421f8d7f729873c9a6ad7c95d8
canepat <16927169+canepat@users.noreply.github.com>
2024-12-09 16:03:31 +0100

    remove debug logs

--- silkworm/node/stagedsync/stages/stage_triggers.cpp
+++ silkworm/node/stagedsync/stages/stage_triggers.cpp
@@ -29,37 +29,28 @@ TriggersStage::TriggersStage(SyncContext* sync_context)
 
 Stage::Result TriggersStage::forward(db::RWTxn& tx) {
     current_tx_ = &tx;
-    SILK_INFO_M("TriggersStage", {"op", "Forward", "current_tx_", std::to_string(intptr_t(current_tx_))}) << "START";
     [[maybe_unused]] auto _ = gsl::finally([this] {
-        SILK_INFO_M("TriggersStage", {"op", "Forward", "current_tx_", std::to_string(intptr_t(current_tx_))}) << "finally";
         current_tx_ = nullptr;
     });
 
     ioc_.restart();
-    SILK_INFO_M("TriggersStage", {"op", "Forward", "current_tx_", std::to_string(intptr_t(current_tx_))}) << "AFTER restart";
     ioc_.run();
 
-    SILK_INFO_M("TriggersStage", {"op", "Forward", "current_tx_", std::to_string(intptr_t(current_tx_))}) << "END";
     return Stage::Result::kSuccess;
 }
 
 Task<void> TriggersStage::schedule(std::function<void(db::RWTxn&)> callback) {
     auto task_caller = [this, c = std::move(callback)]() -> Task<void> {
         db::RWTxn* tx = this->current_tx_;
-        SILK_INFO_M("TriggersStage", {"op", "schedule", "current_tx_", std::to_string(intptr_t(tx))}) << "callback";
-        // SILKWORM_ASSERT(tx);  // TODO(canepat) debug why this get hit after restart
-        if (tx == nullptr) co_return;
+        SILKWORM_ASSERT(tx);
         c(*tx);
         co_return;
     };
-    SILK_INFO_M("TriggersStage", {"op", "schedule", "current_tx_", std::to_string(intptr_t(current_tx_))}) << "BEFORE spawn";
     co_return co_await concurrency::spawn_task(ioc_, task_caller());
 }
 
 bool TriggersStage::stop() {
-    SILK_INFO_M("TriggersStage", {"op", "stop", "current_tx_", std::to_string(intptr_t(current_tx_))}) << "START";
     ioc_.stop();
-    SILK_INFO_M("TriggersStage", {"op", "stop", "current_tx_", std::to_string(intptr_t(current_tx_))}) << "END";
     return Stage::stop();
 }
 
